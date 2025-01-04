18ab3d063c68908a51b97be9ede667930fa6cf8e
Abdallah Aburomi <abdlaa1912@gmail.com>
2025-01-04 03:51:43 +0200

    Parking button navigates to the park selection fragment

---
+++ app/src/main/java/com/example/parking/ui/parking/ParkData.java
@@ -0,0 +1,11 @@
+package com.example.parking.ui.parking;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class ParkData {
+    public static List<String> CITIES = Arrays.asList("city1", "city2", "city3");
+    public static Map<String, List<String>> PARK_MAP = new HashMap<>();
+}
